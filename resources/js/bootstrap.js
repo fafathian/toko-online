048054acf931c49e3068474f2f14394d2471bc1e
fafathian <fafathian9@gmail.com>
2026-04-15 21:58:19 +0700

    hardcode pusher public key to bypass docker vite cache

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,11 +18,11 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'ap1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    key: 'd1f54c65684f8c66a509',
+    cluster: 'ap1',
+    wsHost: 'ws-ap1.pusher.com',
+    wsPort: 80,
+    wssPort: 443,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
